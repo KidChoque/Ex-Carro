20fc581f34e0daf6940a910e6d0990c90b398967
KidChoque <lucassmachado1998@gmail.com>
2023-05-11 08:59:40 -0300

    Treinando orientação a objetos usando list e construtores

---
+++ Carro.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace carro
+{
+    public class Carro
+    {
+        public string Marca;
+        public string Cor;
+
+        public Carro()
+        {
+
+        } 
+
+        public Carro(string marca, string cor)
+        {
+            Marca = marca;
+            Cor = cor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using carro;
+
+List<Carro> carros = new List<Carro>();
+
+for (var i = 0; i < 2; i++)
+{
+    
+Carro carro = new Carro();
+Console.WriteLine($"Qual a marca do carro ?");
+carro.Marca = Console.ReadLine();
+
+Console.WriteLine($"Qual a cor do carro ?");
+carro.Cor = Console.ReadLine();
+
+carros.Add(
+
+new Carro( carro.Marca, carro.Cor ));
+}
+    
+
+
+foreach (var item in carros)
+{
+    Console.WriteLine($"Marca:{item.Marca},Cor:{item.Cor}");
+    
+}
+
